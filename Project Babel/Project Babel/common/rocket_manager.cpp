a2447a4925bf06461154256a3e6dfe41b282fdb2
aaether14 <aaether14@yahoo.com>
2015-03-09 04:35:44 -1000

    Librocket 40%

--- Project Babel/Project Babel/common/rocket_manager.cpp
+++ Project Babel/Project Babel/common/rocket_manager.cpp
@@ -14,6 +14,44 @@ float RocketSystemManager::GetElapsedTime()
 }
 
 
+bool RocketSystemManager::LogMessage(Rocket::Core::Log::Type type, const Rocket::Core::String& message)
+{
+
+
+
+	std::string Type;
+
+	switch (type)
+	{
+	case Rocket::Core::Log::LT_ALWAYS:
+		Type = "[Always]";
+		break;
+	case Rocket::Core::Log::LT_ERROR:
+		Type = "[Error]";
+		break;
+	case Rocket::Core::Log::LT_ASSERT:
+		Type = "[Assert]";
+		break;
+	case Rocket::Core::Log::LT_WARNING:
+		Type = "[Warning]";
+		break;
+	case Rocket::Core::Log::LT_INFO:
+		Type = "[Info]";
+		break;
+	case Rocket::Core::Log::LT_DEBUG:
+		Type = "[Debug]";
+		break;
+	};
+
+	printf("%s - %s\n", Type.c_str(), message.CString());
+
+	return true;
+
+
+
+}
+
+
 
 void RocketManager::Install()
 {
@@ -32,8 +70,12 @@ void RocketManager::Install()
 
 
 
-void RocketRenderManager::Init()
+void RocketRenderManager::Init(Controller * ctrl)
 {
+
+
+	this->ctrl = ctrl;
+
 
 
 	glGenVertexArrays(1, &this->m_VAO);
@@ -45,7 +87,9 @@ void RocketRenderManager::Init()
 }
 
 
-void RocketRenderManager::RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation)
+void RocketRenderManager::RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices,
+	int* indices, int num_indices, Rocket::Core::TextureHandle texture, 
+	const Rocket::Core::Vector2f& translation)
 {
 
 
@@ -64,6 +108,12 @@ void RocketRenderManager::RenderGeometry(Rocket::Core::Vertex* vertices, int num
 		Colors[i] = vertices[i].colour;
 		TexCoords[i] = vertices[i].tex_coord;
 	};
+
+
+	glUseProgram(this->m_program);
+
+
+	glUniform2f(this->offset_uniform_location, translation.x, translation.y);
 
 
 	glBindVertexArray(this->m_VAO);
@@ -88,11 +138,51 @@ void RocketRenderManager::RenderGeometry(Rocket::Core::Vertex* vertices, int num
 	glVertexAttribPointer(COLOR_LOCATION, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->m_VBOs[INDEX_BUFFER]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * num_indices, indices, GL_STATIC_DRAW);
+
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 
 	glDrawElements(GL_TRIANGLES, num_indices, GL_UNSIGNED_INT, indices);
 
 
+
+	glDisable(GL_BLEND);
+
+
 	glBindVertexArray(0);
+
+
+
+	glUseProgram(0);
+
+}
+
+
+
+
+void RocketRenderManager::EnableScissorRegion(bool enable)
+{
+
+
+	if (enable)
+		glEnable(GL_SCISSOR_TEST);
+	else
+		glDisable(GL_SCISSOR_TEST);
+
+
+}
+
+
+
+void RocketRenderManager::SetScissorRegion(int x, int y, int width, int height)
+{
+
+
+	glScissor(x, this->ctrl->GetWindowHeight() - (y + height), width, height);
+
 
 }
--- Project Babel/Project Babel/common/rocket_manager.hpp
+++ Project Babel/Project Babel/common/rocket_manager.hpp
@@ -30,6 +30,9 @@ class RocketSystemManager : public Rocket::Core::SystemInterface
 	virtual float GetElapsedTime();
 
 
+	virtual bool LogMessage(Rocket::Core::Log::Type type, const Rocket::Core::String& message);
+
+
 };
 
 
@@ -45,28 +48,62 @@ class RocketRenderManager : public Rocket::Core::RenderInterface
 	enum m_VBOs
 	{
 
+		INDEX_BUFFER,
 		POS_VBO,
 		TEX_VBO,
-		COL_VBO
+		COL_VBO,
+		NUM_VBS
 
 
 
 	};
 
 
-	GLuint m_VBOs[3];
+	GLuint m_VBOs[NUM_VBS];
 
 
 	GLuint m_VAO;
 
 
-	virtual void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation);
+	GLuint m_program;
 
 
-	inline RocketRenderManager(){ this->Init(); }
+	GLuint offset_uniform_location;
 
 
-	void Init();
+	Controller * ctrl;
+
+
+	virtual void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, 
+		int* indices, int num_indices, Rocket::Core::TextureHandle texture, 
+		const Rocket::Core::Vector2f& translation);
+
+
+	virtual void EnableScissorRegion(bool enable);
+
+
+	virtual void SetScissorRegion(int x, int y, int width, int height);
+
+
+
+	virtual bool LoadTexture(Rocket::Core::TextureHandle& texture_handle,
+		Rocket::Core::Vector2i& texture_dimensions,
+		const Rocket::Core::String& source);
+
+
+	virtual bool GenerateTexture(Rocket::Core::TextureHandle& texture_handle,
+		const Rocket::Core::byte* source,
+		const Rocket::Core::Vector2i& source_dimensions);
+
+
+	virtual void ReleaseTexture(Rocket::Core::TextureHandle texture_handle);
+
+
+
+	inline RocketRenderManager(Controller * ctrl){ this->Init(ctrl); }
+
+
+	void Init(Controller * ctrl);
 
 
 };
