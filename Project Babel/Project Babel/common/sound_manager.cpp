cae728a875282dfa3cdbd5eaa75352ae1637f815
Zexon Prime <zexonprime@outlook.com>
2015-03-15 06:22:43 -0700

    Music

--- Project Babel/Project Babel/common/sound_manager.cpp
+++ Project Babel/Project Babel/common/sound_manager.cpp
@@ -3,14 +3,16 @@
 void SoundManager::Init()
 {
 
-	this->Sounds = new std::vector<Sound*>;
+	this->m_sounds = new std::vector<Sound*>;
 
+	this->m_music = new std::vector<Music*> ;
 
 	char** sound_path = new char*[3];
 
 
-	sound_path[0] = "./media/horn.wav";
-	AddSound(sound_path[0], "Horn");
+	sound_path[0] = "./media/spooky.ogg";
+	AddSound(sound_path[0], "Spooky");
+	
 
 }
 
@@ -18,12 +20,12 @@ void SoundManager::Clean()
 {
 
 
-	delete this->Sounds;
+	delete this->m_sounds;
 
 
 }
 
-void SoundManager::AddSound(char* SoundPath, char *name)
+void SoundManager::AddSound(char* p_sound, char *name)
 {
 	Sound *t_sound = new Sound;
 
@@ -31,8 +33,8 @@ void SoundManager::AddSound(char* SoundPath, char *name)
 
 
 
-	if (!buffer->loadFromFile(SoundPath))
-		printf("ERROR: Couldn't initialize buffer!!! at %d \n\n", SoundPath);
+	if (!buffer->loadFromFile(p_sound))
+		printf("ERROR: Couldn't load sound buffer from %d \n\n", p_sound);
 
 
 
@@ -43,19 +45,53 @@ void SoundManager::AddSound(char* SoundPath, char *name)
 
 
 	t_sound->buffer = buffer;
+
+
 	sound->setBuffer(*t_sound->buffer);
+
+
 	t_sound->sound = sound;
+
+
 	t_sound->name = name;
 
+
+	t_sound->volume = t_sound->sound->getVolume();
 
 
 	t_sound->sound->play();
 
+	this->m_sounds->push_back(t_sound);
 
 
-	this->Sounds->push_back(t_sound);
+
+
+}
+
+
+void SoundManager::AddMusic(char* p_music, char* name)
+{
+	
+	Music *t_music = new Music;
 
 
 
+	sf::Music * music = new sf::Music;
+
+
+	if (music->openFromFile(p_music))
+		printf("ERROR: Couldn't load music from %d \n\n", p_music);
+
+
+	t_music->music = music;
+
+
+	t_music->name = name;
+
+
+	t_music->volume = t_music->music->getVolume();
+
+
+	this->m_music->push_back(t_music);
 
 }
--- Project Babel/Project Babel/common/sound_manager.hpp
+++ Project Babel/Project Babel/common/sound_manager.hpp
@@ -13,11 +13,27 @@ class SoundManager
 		sf::SoundBuffer *buffer;
 		sf::Sound *sound;
 
+		float volume;
+
 		char *name;
 	};
 
-	std::vector<Sound*> *Sounds;
 
+
+	struct Music{
+		sf::Music *music;
+
+		float volume;
+
+		char *name;
+	};
+
+
+
+
+	std::vector<Sound*> *m_sounds;
+
+	std::vector<Music*> *m_music;
 
 public:
 
@@ -29,9 +45,18 @@ public:
 
 	void Clean();
 
-	void AddSound(char* Sound, char* name);
+	void AddSound(char* p_sound, char* name);
 
-	inline std::vector<Sound*> *GetSounds(){ return this->Sounds; }
+
+	void AddMusic(char* p_music, char* name);
+
+
+	inline std::vector<Sound*> *GetSounds(){ return this->m_sounds; }
+
+
+	inline std::vector<Music*> *GetMusic(){ return this->m_music; }
+
+
 };
 
 
